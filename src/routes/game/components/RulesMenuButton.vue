9e64a1ae8676a94728b1302d8d9099ba52582e6b
TobiasAndren <tobiasandren@hotmail.com>
2025-10-01 14:03:13 +0200

    Moved rules button out of game menu and to a seperate button

---
+++ src/routes/game/components/RulesMenuButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-btn id="rules-button" icon variant="text" aria-label="Show Rules" @click="openDialog">
+      <v-icon color="neutral-lighten-2" icon="mdi-information" aria-hidden="true"></v-icon>
+
+      <RulesDialog v-model="showDialog" @open="onOpen" @close="onClose"></RulesDialog>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import RulesDialog from './dialogs/components/RulesDialog.vue';
+
+export default {
+  name: 'RulesButton',
+  components: { RulesDialog },
+  data() {
+    return {
+      showDialog: false,
+    };
+  },
+  methods: {
+    openDialog() {
+      this.showDialog = true;
+    },
+    onOpen() {
+      this.$emit('open');
+    },
+    onClose() {
+      this.showDialog = false;
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
