093d3f0cd68e3baf22d31827198ea7fd3c892a64
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-03 10:42:19 +0200

    Update: Color on close button, also a better centration of buttons in right menu and removal of spec number

--- src/routes/game/components/SpectatorListMenu.vue
+++ src/routes/game/components/SpectatorListMenu.vue
@@ -12,13 +12,7 @@
         aria-label="View current spectators"
         title="View current spectators"
       >
-        <v-icon
-          class="mr-1"
-          icon="mdi-eye-outline"
-          size="large"
-          aria-hidden="true"   
-        />
-        <span v-if="!$vuetify.display.xs" class="pr-2">{{ spectatingUsers.length }}</span>
+        <v-icon class="mr-1" icon="mdi-eye-outline" size="large" aria-hidden="true" />
       </v-btn>
     </template>
     <!-- Menu -->
